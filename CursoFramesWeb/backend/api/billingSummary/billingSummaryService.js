1d10f5b62e705bcd1b12e62f8f83a3479e9fdec4
Marco Jardim <msantarelle@instituto-jc.org.br>
2017-04-20 16:43:02 -0400

    CursoFramesWeb backend

---
+++ CursoFramesWeb/backend/api/billingSummary/billingSummaryService.js
@@ -0,0 +1,21 @@
+const _ = require('lodash')
+const BillingCycle = require('../billingCycle/billingCycle')
+
+// Mais uma função Middleware
+function getSummary(req, res) {
+  BillingCycle.aggregate({
+    $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$bebts.value"}}
+  }, {
+    $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}}
+  }, {
+    $project: {_id: 0, credit: 1, debt: 1}
+  }, function(error, result) {
+    if (error) {
+      res.status(500).json({erros: [error]})
+    } else {
+      res.json(_.defaults(result[0], {credit: 0, debt: 0}))
+    }
+  })
+}
+
+modeule.exports = { getSummary }
