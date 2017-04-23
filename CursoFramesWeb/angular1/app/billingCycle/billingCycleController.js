e9a084938f0d28710e15ddca13f53fc039bf0d4f
Marco Jardim <santarelle@gmail.com>
2017-04-23 14:41:36 -0400

    finalizado a parte de alterar e excluir um ciclo de pagamento

--- CursoFramesWeb/angular1/app/billingCycle/billingCycleController.js
+++ CursoFramesWeb/angular1/app/billingCycle/billingCycleController.js
@@ -34,7 +34,27 @@
 
     vm.showTabDelete = function(billingCycle) {
       vm.billingCycle = billingCycle
-      tabs.show(vm, {tabUpdate: true})
+      tabs.show(vm, {tabDelete: true})
+    }
+
+    vm.update = function() {
+      const updateUrl = `${url}/${vm.billingCycle._id}`
+      $http.put(updateUrl, vm.billingCycle).then(function(response) {
+        vm.refresh()
+        msgs.addSuccess('Operação realizada com sucesso!')
+      }).catch(function(response) {
+        msgs.addError(response.data.errors)
+      })
+    }
+
+    vm.delete = function() {
+      const deleteUrl = `${url}/${vm.billingCycle._id}`
+      $http.delete(deleteUrl, vm.billingCycle).then(function(response) {
+        vm.refresh()
+        msgs.addSuccess('Operação realizada com sucesso!')
+      }).catch(function(response) {
+        msgs.addError(response.data.errors)
+      })
     }
 
     vm.refresh()
